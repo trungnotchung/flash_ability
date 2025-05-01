a2c473ef8f3986d33d37580e15966081a375e444
trungnotchung <trung09072004@gmail.com>
2025-05-01 22:10:23 +0700

    add mock data for search page

--- lib/mock_data/search/search_vocab.dart
+++ lib/mock_data/search/search_vocab.dart
@@ -1,30 +1,62 @@
 List<Map<String, String>> searchVocabData = [
   {
     'word': 'address',
+    'type': 'n', // Noun
     'description':
         'The number of the house or building, and the street name where someone lives or works.',
   },
   {
     'word': 'address',
+    'type': 'n', // Noun
     'description':
         'A series of letters and symbols that represent a location, used in emails and websites.',
   },
   {
     'word': 'address',
+    'type': 'n', // Noun
     'description': 'A formal speech delivered to an audience.',
   },
   {
     'word': 'apple',
+    'type': 'n', // Noun
     'description': 'A round fruit with red or green skin and a white interior.',
   },
   {
     'word': 'banana',
+    'type': 'n', // Noun
     'description':
         'A long, curved fruit with a yellow skin and soft, sweet flesh.',
   },
   {
     'word': 'grape',
+    'type': 'n', // Noun
     'description':
         'A small, round fruit that grows in clusters, typically green, purple, or red.',
+  },
+  {
+    'word': 'run',
+    'type': 'v', // Verb
+    'description': 'To move swiftly on foot.',
+  },
+  {
+    'word': 'quick',
+    'type': 'adj', // Adjective
+    'description': 'Moving fast or able to move fast.',
+  },
+  {
+    'word': 'happiness',
+    'type': 'n', // Noun
+    'description': 'The state of being happy or feeling pleasure.',
+  },
+  {
+    'word': 'beautiful',
+    'type': 'adj', // Adjective
+    'description': 'Pleasing the senses or mind aesthetically.',
+  },
+  {
+    'word': 'swim',
+    'type': 'v', // Verb
+    'description':
+        'To move through water by making movements with the arms and legs.',
   },
 ];
