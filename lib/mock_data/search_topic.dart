0ffbaa3374470ff21c68ac15dbb23b91af3059ac
trungnotchung <trung09072004@gmail.com>
2025-05-01 21:51:10 +0700

    update search mock data

---
+++ lib/mock_data/search_topic.dart
@@ -0,0 +1,31 @@
+List<Map<String, String>> searchTopicData = [
+  {
+    'topic': 'Fruit',
+    'description':
+        'A category of edible items that are typically sweet and nutritious.',
+  },
+  {
+    'topic': 'Food',
+    'description':
+        'Any substance consumed to provide nutritional support for the body.',
+  },
+  {
+    'topic': 'Vegetables',
+    'description': 'Plants or parts of plants that are consumed as food.',
+  },
+  {
+    'topic': 'Animals',
+    'description':
+        'Living organisms that typically have the ability to move, respond to stimuli, and feed.',
+  },
+  {
+    'topic': 'Technology',
+    'description':
+        'The application of scientific knowledge for practical purposes.',
+  },
+  {
+    'topic': 'Sports',
+    'description':
+        'Physical activities that involve skill, competition, and athleticism.',
+  },
+];
---
+++ lib/mock_data/search_vocab.dart
@@ -0,0 +1,30 @@
+List<Map<String, String>> searchVocabData = [
+  {
+    'word': 'address',
+    'description':
+        'The number of the house or building, and the street name where someone lives or works.',
+  },
+  {
+    'word': 'address',
+    'description':
+        'A series of letters and symbols that represent a location, used in emails and websites.',
+  },
+  {
+    'word': 'address',
+    'description': 'A formal speech delivered to an audience.',
+  },
+  {
+    'word': 'apple',
+    'description': 'A round fruit with red or green skin and a white interior.',
+  },
+  {
+    'word': 'banana',
+    'description':
+        'A long, curved fruit with a yellow skin and soft, sweet flesh.',
+  },
+  {
+    'word': 'grape',
+    'description':
+        'A small, round fruit that grows in clusters, typically green, purple, or red.',
+  },
+];
