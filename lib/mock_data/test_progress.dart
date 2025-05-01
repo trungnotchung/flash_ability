a03bc90d8961a0309626c2ae55b65097b7019d42
trungnotchung <trung09072004@gmail.com>
2025-05-01 22:18:58 +0700

    add mock data for learning page

--- lib/mock_data/test_progress.dart
+++
@@ -1,5 +0,0 @@
-List<Map<String, dynamic>> testProgress = [
-  {'testName': 'Family Vocabulary Test', 'progress': '75%', 'status': 'Completed'},
-  {'testName': 'Animal Vocabulary Test', 'progress': '50%', 'status': 'In Progress'},
-  {'testName': 'Technology Vocabulary Test', 'progress': '20%', 'status': 'Not Started'}
-];
