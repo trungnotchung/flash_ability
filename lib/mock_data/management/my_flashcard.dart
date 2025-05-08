8f8673282194953bcce56a0c41fd0e4f68a510d0
trungnotchung <trung09072004@gmail.com>
2025-05-08 21:15:11 +0700

    update my flashcard mock data

--- lib/mock_data/management/my_flashcard.dart
+++ lib/mock_data/management/my_flashcard.dart
@@ -1,5 +1,20 @@
 List<Map<String, String>> flashcardGroups = [
-  {'groupName': 'Group 1', 'numberOfCards': '50'},
-  {'groupName': 'Group 2', 'numberOfCards': '30'},
-  {'groupName': 'Group 3', 'numberOfCards': '20'},
+  {
+    'groupName': 'Group 1',
+    'numberOfCards': '12',
+    'words':
+        'Father,Mother,Sister,Brother,Grandmother,Grandfather,Teacher,Student,Classroom,Homework,Notebook,Chalkboard',
+  },
+  {
+    'groupName': 'Group 2',
+    'numberOfCards': '12',
+    'words':
+        'Lion,Elephant,Tiger,Giraffe,Zebra,Monkey,Football,Basketball,Tennis,Swimming,Running,Cycling',
+  },
+  {
+    'groupName': 'Group 3',
+    'numberOfCards': '10',
+    'words':
+        'Force,Energy,Mass,Velocity,Acceleration,Momentum,Engineer,Doctor,Teacher,Lawyer',
+  },
 ];
